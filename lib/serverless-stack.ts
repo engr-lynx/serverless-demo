164743eb2e38898a322738a1f2d969d4a1869964
Leon Valencia <techie@lynx.rocks>
2021-05-08 13:25:59 +0800

    separate serverless-stack from cicd-demo

---
+++ lib/serverless-stack.ts
@@ -0,0 +1,36 @@
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { Repository } from '@aws-cdk/aws-ecr';
+import { DockerImageFunction, DockerImageCode } from '@aws-cdk/aws-lambda';
+import { AuthorizationType } from '@aws-cdk/aws-apigateway';
+import { ApiGatewayToLambda } from '@aws-solutions-constructs/aws-apigateway-lambda';
+
+export interface ServerlessProps extends StackProps {
+  imageRepoName: string,
+}
+
+export class ServerlessStack extends Stack {
+
+  constructor(scope: Construct, id: string, serverlessProps?: ServerlessProps) {
+    if (serverlessProps == null) {
+      return
+    }
+    super(scope, id, serverlessProps);
+    const imageRepo = Repository.fromRepositoryName(this, 'ImageRepo', serverlessProps.imageRepoName);
+    const lambdaImage = DockerImageCode.fromEcr(imageRepo);
+    const lambdaObj = new DockerImageFunction(this, 'LambdaObj', {
+      code: lambdaImage,
+    });
+    const methodOpts = {
+      // ToDo: change type to custom or Cognito once the auth mechanism is added
+      authorizationType: AuthorizationType.NONE,
+    };
+    const apiGatewayProps = {
+      defaultMethodOptions: methodOpts,
+    }
+    new ApiGatewayToLambda(this, 'Serverless', {
+      existingLambdaObj: lambdaObj,
+      apiGatewayProps,
+    });
+  }
+
+}
--- test/serverless-demo.test.ts
+++ test/serverless-demo.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as ServerlessDemo from '../lib/serverless-demo-stack';
+import { App } from '@aws-cdk/core';
+import { ServerlessStack } from '../lib/serverless-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
-    const stack = new ServerlessDemo.ServerlessDemoStack(app, 'MyTestStack');
+    const stack = new ServerlessStack(app, 'ServerlessDemo');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
