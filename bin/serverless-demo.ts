fb54f08911726d9be1f1d55f22b4034237109923
Leon Valencia <techie@lynx.rocks>
2021-05-17 21:40:13 +0800

    fix issue with importing VPC

--- bin/serverless-demo.ts
+++ bin/serverless-demo.ts
@@ -4,10 +4,16 @@ import { App } from '@aws-cdk/core';
 import { ServerlessStack } from '../lib/serverless-stack';
 
 const app = new App();
+// using VPC from another app needs the environment to be set explicitly
+const appEnv = {
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+};
 const stackId = app.node.tryGetContext('stackId');  
 const repoName = app.node.tryGetContext('repoName');
 const vpcId = app.node.tryGetContext('vpcId');
 new ServerlessStack(app, stackId, {
   repoName,
   vpcId,
+  env: appEnv,
 });
